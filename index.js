66cf4c422cb50eb251c9c3d4b23e677b9f148d89
hansenjl <jenn.leigh.hansen@gmail.com>
2020-08-05 18:47:38 -0700

    set up for study group

---
+++ index.js
@@ -0,0 +1,7 @@
+//As a developer I want...
+
+//a form to appear in place of the 'Add Emoji' button when the 'Add Emoji' button is clicked
+//an emoji can be entered into the input, submitted, and added to the "Emoji Bank"
+//an alert saying "Emoji Submitted!" whenever an emoji is submitted
+//the form turns back into the 'Add Emoji' button once an emoji has been successfully submitted
+//an emoji I click on turns into '🤪'
